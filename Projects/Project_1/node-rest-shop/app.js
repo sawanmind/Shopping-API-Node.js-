a5d5cba7ded9df7c3d1ec4ad3ac65bde4f1af4e2
iOS Development <iosdevelopment@iOSs-Mac-mini.local>
2018-05-09 16:15:18 +0530

    node.js init

---
+++ Projects/Project_1/node-rest-shop/app.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const productsRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+
+app.use('/products', productsRoutes)
+app.use('/orders', orderRoutes)
+
+app.use((req,res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error : {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
